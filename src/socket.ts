f80ed62f3547dfc5486dbdc6a6a4c2e394f5aad8
Rogelio Gutierrez <gutierrez.rog12@gmail.com>
2023-04-14 11:31:47 -0300

    feat: display connection status

--- src/socket.ts
+++ src/socket.ts
@@ -1,8 +1,22 @@
-import { Manager } from 'socket.io-client'
+import { Manager, Socket } from 'socket.io-client'
 import { SOCKET_URL } from './utils/constants'
 
 export const connect = () => {
   const manager = new Manager(SOCKET_URL)
 
   const socket = manager.socket('/')
+
+  addListeners(socket)
+}
+
+const addListeners = (socket: Socket) => {
+  const connectionStatus = document.querySelector('#connection-status')!
+
+  socket.on('connect', () => {
+    connectionStatus.innerHTML = 'Online'
+  })
+
+  socket.on('disconnect', () => {
+    connectionStatus.innerHTML = 'Offline'
+  })
 }
